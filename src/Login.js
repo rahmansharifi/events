8fca6d994b06fc1a4c0bc503be93af25f9128468
Rahman <sharifi.rahman.4651@gmail.com>
2022-08-05 08:29:44 +0430

    Signup and Login connected to api; time to develop  Dashboard

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,116 @@
-import React from 'react';
+import React, { useState, useRef, /*useEffect*/ } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
 
 const Login = () => {
+
+    const forward = useNavigate();
+    const cookies = new Cookies();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [loader, setLoader] = useState(false);
+    const [error, setError] = useState('');
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    async function submit(event) {
+        event.preventDefault()
+        emailRef.current.style.border = '1px solid #ffffff';
+        passwordRef.current.style.border = '1px solid #ffffff';
+        const regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
+        if (!email) {
+            setError('Email cant be empty')
+            emailRef.current.style.border = '1px solid #ff0000';
+            emailRef.current.style.boxShadow = 'none';
+            emailRef.current.focus();
+            return null;
+        }
+        else if (!regex.test(email)) {
+            setError('Address is invalid')
+            emailRef.current.style.border = '1px solid #ff0000';
+            emailRef.current.style.boxShadow = 'none';
+            emailRef.current.focus();
+            return null;
+        }
+        else if (!password) {
+            setError('Password cant be empty')
+            passwordRef.current.style.border = '1px solid #ff0000';
+            passwordRef.current.style.boxShadow = 'none';
+            passwordRef.current.focus();
+            return null;
+        }
+        else if (password.length < 6 ) {
+            setError('Enter 6 chars at least')
+            passwordRef.current.style.border = '1px solid #ff0000';
+            passwordRef.current.style.boxShadow = 'none';
+            passwordRef.current.focus();
+            return null;
+        }
+        setError('')
+        setLoader(true)
+        try {
+            const call = await axios.post('https://api.rahmansharifi.ir/login',{
+                email: email,
+                password: password,
+            })
+            if (call.data.http === 200) {
+                cookies.set('auth',call.data.auth,{maxAge:7*24*60*60})
+                forward('/dashboard')
+            } 
+            else
+            {
+                throw new Error(call.data.exception)
+            }
+        } catch(e) {
+            setLoader(false)
+            setError((''+e).split('\n')[0].replace('Error: ','').replace('<email>','Email').replace('<password>','Password'))
+        }
+    }
+
     return (
         <div className='form-container'>
             <form>
                 <h1>Login</h1>
-                <input name='events_todo_email' type='email' placeholder='Email' />
-                <input name='events_todo_password' type='password' placeholder='Password' />
+                <input 
+                    required
+                    ref={emailRef}
+                    name='events_todo_email' 
+                    type='email' 
+                    placeholder='Email' 
+                    value={email}
+                    onChange={event=>setEmail(event.target.value)}
+                />
+                <input 
+                    required
+                    ref={passwordRef}
+                    name='events_todo_password' 
+                    type='password' 
+                    placeholder='Password' 
+                    value={password}
+                    onChange={event=>setPassword(event.target.value)}
+                />
                 <div>
-                    <input type='submit' value='Login' />
+                    <input type='submit' value='Login' onClick={submit} />
+                    {
+                        loader &&
+                            <div className='loader'>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                    }
+                    {
+                        error &&
+                            <div className='error'>{error}</div>
+                    }
                     <Link to='/signup'>Signup</Link>
                 </div>
             </form>
